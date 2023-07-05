b62b2e86aa6b3ffd734b143d1d277d1624815106
Brian Szmyd <brian.szmyd@protonmail.com>
2023-07-05 17:25:41 -0600

    Initial freestanding bin.

---
+++ README.md
@@ -0,0 +1,3 @@
+# An OS implemented in Rust
+
+This will begin as a follow-thru of [BlogOS](https://os.phil-opp.com) and continue on to something else.
---
+++ src/main.rs
@@ -0,0 +1,16 @@
+// No Standard Library and no c-runtime
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
